190c52e571d5edd56720836e81556f5f33c92f7d
devkeydet <devkeydet@gmail.com>
2017-11-14 13:58:28 -0500

    Resolves #12

---
+++ WebResources/testhelpers/fakedocready.js
@@ -0,0 +1,10 @@
+﻿//  used to prevent:
+//      $(document).ready(onReady); 
+//  ...from throwing an exception when unit testing
+
+var $ = function (p){
+    return $;
+}
+
+$.ready = function (p){
+}
